7df347826cd0e14a37f45ffcff4fc992e70c51eb
Andrew <andrewstaveley14@gmail.com>
2023-10-07 22:50:55 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (response) {
+      let array = response
+      renderBooks(array)
+    })
 }
 
 function renderBooks(books) {
